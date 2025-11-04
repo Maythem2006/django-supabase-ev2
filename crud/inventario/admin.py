95fb58f6f24bfb2b066053f0b42d2f107312da56
Maythem11O6 <maythem.maldonado@inacapmail.cl>
2025-11-03 21:14:47 -0300

    conexión con supabase realizada
    Ya esta lista la conexión con supabase ahora hay que levantar el servidor (Con Railway o Render)

--- crud/inventario/admin.py
+++
@@ -1,5 +0,0 @@
-#Registrar el modelo Producto para volverlo accesible desde el panel de administración
-from django.contrib import admin
-from .models import Producto
-
-admin.site.register(Producto)
